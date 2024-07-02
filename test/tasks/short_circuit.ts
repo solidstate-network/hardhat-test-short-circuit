a49820d84929d21f0b8fd58acb3a23dc19a58cb4
Nick Barry <itsnickbarry@protonmail.ch>
2024-07-02 22:49:17 +0400

    test short circuit call outside of running mocha suite

--- test/tasks/short_circuit.ts
+++ test/tasks/short_circuit.ts
@@ -2,14 +2,30 @@ import child_process from 'child_process';
 
 const TASK_SHORT_CIRCUIT = 'short-circuit';
 
+let noTestsRunningError: any;
+
+// attempt to call the short circuit task outside of the test suit
+// with the expectation that it will fail
+try {
+  child_process.execSync(`yarn run hardhat ${TASK_SHORT_CIRCUIT}`);
+} catch (error) {
+  noTestsRunningError = error;
+}
+
 describe(TASK_SHORT_CIRCUIT, () => {
-  describe('before task execution', () => {
-    it('does not interfere with tests', async () => {
+  describe('before test suite execution', () => {
+    it('throws an error', async () => {
+      if (!(noTestsRunningError instanceof Error)) {
+        throw new Error('Pre-mocha short circuit did not throw an error');
+      }
+    });
+
+    it('does not interfere with subsequent tests', async () => {
       // this test should pass
     });
   });
 
-  describe('after task execution', () => {
+  describe('during test suite execution', () => {
     before(async () => {
       // run short circuit task in separate process to simulate real-world usage
       await new Promise<void>((resolve) => {

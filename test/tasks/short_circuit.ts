bf1f2e31173da26f4762d0b797fa58e2b36ac496
Nick Barry <itsnickbarry@protonmail.ch>
2024-07-02 22:50:59 +0400

    throw error at end of suite if test is incorrectly skipped

--- test/tasks/short_circuit.ts
+++ test/tasks/short_circuit.ts
@@ -3,6 +3,7 @@ import child_process from 'child_process';
 const TASK_SHORT_CIRCUIT = 'short-circuit';
 
 let noTestsRunningError: any;
+let uninterruptedTestPassed = false;
 
 // attempt to call the short circuit task outside of the test suit
 // with the expectation that it will fail
@@ -21,7 +22,8 @@ describe(TASK_SHORT_CIRCUIT, () => {
     });
 
     it('does not interfere with subsequent tests', async () => {
-      // this test should pass
+      // this test should pass; checked in `after` block
+      uninterruptedTestPassed = true;
     });
   });
 
@@ -38,5 +40,9 @@ describe(TASK_SHORT_CIRCUIT, () => {
     it('skips tests (✔ this test should be skipped)', async () => {
       throw new Error('Failed to short circuit test execution');
     });
+  });
+
+  after(async () => {
+    if (!uninterruptedTestPassed) throw new Error('Unexpectedly skipped test');
   });
 });
